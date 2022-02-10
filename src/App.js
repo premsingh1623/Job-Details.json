7555999b506a2a443f9423b98afcfa21e55430ae
premsingh1623 <premsingh16723@gmail.com>
2022-02-10 20:35:16 +0530

    f

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import { useState } from 'react';
+import Modal from './Components/Modal';
+
 
 function App() {
+
+  const [Name, setName] = useState("")
+  const [Discription, setDiscription] = useState("")
+  const [display, setdisplay] = useState({
+    display: "none"
+  })
+ 
+  const openModal = (Name, Discription, Salary) => {
+    setName(Name)
+    setDiscription(Discription)
+    setdisplay({
+      display: "block"
+    })
+      }
+
+      const closeModal = () => {
+        setdisplay({
+          display: "none"
+        })
+            }
+
   return (
     <>
     <Navbar />
-    <Home />
+    <Home openModal ={openModal} />
+    <Modal display = {display} closeModal = {closeModal} Name = {Name} Discription = {Discription} />
     </>
   );
 }
